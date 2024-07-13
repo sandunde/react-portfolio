6d222593b089e8321b7d6021c0e1d53a2d0b0c75
Sandun <sandundimaria7@gmail.com>
2024-07-13 21:41:48 +0530

    changes

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,7 @@
+"use client";
+
 import { Inter } from "next/font/google";
+import { useEffect } from "react";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -9,6 +12,18 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  useEffect(() => {
+    const threeScript = document.createElement("script");
+    threeScript.setAttribute("id", "threeScript");
+    threeScript.setAttribute(
+      "src",
+      "https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js"
+    );
+    console.log(
+      document.getElementsByTagName("head")[0].appendChild(threeScript)
+    );
+  }, []);
+
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
